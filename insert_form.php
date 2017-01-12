57a383fcec288a042bf7a819488df71ea9689135
kobahiro <ahhrhrkk10180512@gmail.com>
2017-01-12 22:18:25 +0900

    Add files via upload

--- insert_form.php
+++ insert_form.php
@@ -12,7 +12,7 @@
 </p>
 <p>
     本文：(140字以内)<br />
-    <textarea name="contents" rows="2" cols="70"></textarea>
+    <textarea name = "contents" rows = "2" cols = "70" maxlength = "140"></textarea>
 </p>
     <?php
     /**
@@ -22,12 +22,22 @@
      * Time: 17:35
      */
     //DB接続
-    require_once '../../htdocs/b_board/DbManager.php';
-    require_once '../../htdocs/b_board/Encode.php';
-    if(isset($_POST['name']) && isset($_POST['contents'])) {
-        if (empty($_POST['name']) || empty($_POST['contents'])) {
-            echo "未入力の項目があります";
-        } else {
+    require_once '/DbManager.php';
+    require_once '/Encode.php';
+
+    if(!isset($_POST['name']) && !isset($_POST['contents'])){
+        echo "名前、本文を入力してください";
+    }else if(!isset($_POST['contents'])){
+        echo "本文を入力してください";
+    }else if(!isset($_POST['name'])) {
+        echo "名前を入力してください";
+    }else{
+       if(mb_strlen($_POST['name']) > 30){
+           echo "名前は30文字以内で入力してください";
+       }else if(mb_strlen($_POST['contents']) > 140){
+           echo "本文は140文字以内で入力してください";
+       }else{
+
             try {
                 $db = getDb();
                 $name = e($_POST['name']);
@@ -38,18 +48,19 @@
                 $stt->bindValue(':contents', $contents);
                 $stt->execute();
                 $db = NULL;
-
             } catch (PDOException $e) {
                 die("エラーメッセージ：{$e->getMessage()}");
             }
-        }
+       }
     }
+
+
     ?>
 
 <p>
     <input type = "submit" value = "登録" />
 </p>
-    <table border="1">
+    <table border = "1">
         <tr>
             <th>名前</th><th>本文</th>
         </tr>
